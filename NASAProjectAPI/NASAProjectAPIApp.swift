4f4d8a7880b2afea812a364d67b37591fb135628
Matthew Garlington <61671107+MatthewGarlington@users.noreply.github.com>
2022-10-30 19:28:16 -0400

    Initial Commit

---
+++ NASAProjectAPI/NASAProjectAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NASAProjectAPIApp.swift
+//  NASAProjectAPI
+//
+//  Created by Matthew Garlington on 10/30/22.
+//
+
+import SwiftUI
+
+@main
+struct NASAProjectAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
